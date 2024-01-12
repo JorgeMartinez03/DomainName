50d5d331f541b4535bfb846b70902b394fc42c9c
JorgeMartinez03 <94927018+JorgeMartinez03@users.noreply.github.com>
2024-01-12 18:25:41 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Datos individuales de los dominios.
+
+  let articles = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let tlds = [".com", ".net"];
+
+  let domainNames = [];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
+
+  const generateButton = document.querySelector("#generate");
+  const domainContainer = document.querySelector("#domain");
+
+  generateButton.onclick = () => {
+    domainContainer.innerHTML = "<h1>Generated Domains:</h1>";
+
+    domainNames.forEach(domain => {
+      const domainElement = document.createElement("p");
+      domainElement.textContent = domain;
+      domainContainer.appendChild(domainElement);
+    });
+  };
 };
